33c8e125873228410f888c45c16356ea227a0e14
George Duncan <gtduncan@usc.edu>
2022-08-11 14:43:54 -0400

    commit tree 5f7ba07863d591305be65d64ea4dfac76974627f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
 
   def count_sentences
-
+    sentenceArray = self.gsub('!', '.').gsub('?', '.').split('.')
+    puts(sentenceArray)
+    sentenceArray.delete('')
+    sentenceArray.count
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
